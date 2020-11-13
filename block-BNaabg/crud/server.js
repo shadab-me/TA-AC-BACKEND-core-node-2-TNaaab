1c93008381a07be3328f6f1c4c2fb0f612590087
Shadab Ali <shadabali604@gmail.com>
2020-11-13 15:56:27 +0530

    added: one user at time

--- block-BNaabg/crud/server.js
+++ block-BNaabg/crud/server.js
@@ -1,30 +1,57 @@
+
 let fs = require('fs');
 let http = require('http');
-let usersPath = __dirname + "/users/"
-
+let usersPath = __dirname + "/users/";
+let url = require('url');
 let server = http.createServer(serverHandler);
 
-
 function serverHandler(req, res){
-  let store = ''
+let parseUrl = url.parse(req.url, true);
+let getName  = parseUrl.query.username;
+ console.log(getName);
+let pathName = parseUrl.pathname;
+console.log(pathName);
+
+
+
+console.log(pathName);
+   let store = ''
 req.on('data', (chunk) => {
     store =  store + chunk;
 });
 
 req.on('end', () => {
-    if(req.method == "POST" && req.url == '/users'){
+    if(req.method == "POST" && pathName == '/users'){
       let name = JSON.parse(store).username;
-      fs.open(usersPath + name + ".json", "wx", (err, nf) => {
-          if(err) console.log(err);
+      console.log(usersPath + name + ".json")
+       fs.open(usersPath + name + ".json", "wx", (err, nf) => {
+          console.log(nf, 'hshhshs');
+           if(err) console.log(err);
           fs.writeFile(nf, store, (err) => {
             if(err) console.log(err);
-           })
-      })
+            fs.close(nf, (err) => {
+                if(err) console.log(err);
+                res.end(`${name} is created successfully.`);
+            });
+           });
+      });
 
-       res.end(store);
-    }
+    }else if(req.method == "GET" && pathName == '/users'){
+       
+        console.log(usersPath + getName + ".json")
+        fs.open(usersPath + getName + ".json", "r", (err, nf) => {
+           if(err) console.log(err);
+           fs.readFile(nf, (err, user) => {
+                if(err) console.log(err);
+                res.end(user);
+            }); 
+     });
+     
+};
+
 });
 }
+
 server.listen(3000, () => {
     console.log('Server is running on 3000')
 });
